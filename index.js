964ab93bd30c455fcfaf235a52d5bb82ce0031ae
BenWix <benwichser@gmail.com>
2020-06-30 17:09:11 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+function titleCased() {
+  return tutorials.map(tut => 
+    tut.split(" ").map(word =>
+      word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    )
 }

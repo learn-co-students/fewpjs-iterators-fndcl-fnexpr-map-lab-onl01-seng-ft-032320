a5058f3387e2a0be4afb1f04f01381d95da81f6d
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-06 13:09:32 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => (
+    tutorial.split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
+  ))
 }

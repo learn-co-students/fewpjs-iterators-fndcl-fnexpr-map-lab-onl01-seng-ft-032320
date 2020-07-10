c31aec9d9296c959d2f2eaf214588db3558796b7
Christopher <reinkendigital@gmail.com>
2020-07-10 11:09:33 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (tutorial) {
+      return tutorial.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+  })
 }
